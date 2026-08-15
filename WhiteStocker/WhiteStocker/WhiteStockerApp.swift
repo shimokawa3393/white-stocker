7788fe60b70c8275fe0c6fc21c1ca7a193ece114
s.shimokawa <ssmjkopapplehonda@icloud.com>
2026-08-15 11:57:23 +0900

    step1完了

--- WhiteStocker/WhiteStocker/WhiteStockerApp.swift
+++ WhiteStocker/WhiteStocker/WhiteStockerApp.swift
@@ -11,8 +11,8 @@ import SwiftData
 @main
 struct WhiteStockerApp: App {
     var sharedModelContainer: ModelContainer = {
+        // TODO: Step 2でTask / Placementモデルを実装したらここに追加する
         let schema = Schema([
-            Item.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
